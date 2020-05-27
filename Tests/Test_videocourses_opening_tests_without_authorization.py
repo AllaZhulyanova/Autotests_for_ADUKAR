6c9e06b2ce5e50b45af517fb6acf437be85908ff
Alla Zhulyanova <AllaZhJod@gmail.com>
2020-05-27 10:03:18 +0300

    В тесте "Доступ к тестам без авторизации" добалены проверки

--- Tests/Test_videocourses_opening_tests_without_authorization.py
+++
@@ -1,7 +0,0 @@
-
-
-# Доступ к тесту без авторизации
-
-def test_opening_test_without_authorization (app):
-    app.Button_menu.Test_Button_Videocourses() # кнопка "Видеокурсы"
-    app.List_items_without_payments.Test_list_items() # выбор случайного предмета
